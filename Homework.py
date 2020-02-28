9207cedc516a0d6797bd67597d7a52974c33153a
Elisabeth Peltier <peltier.elisabeth@yahoo.ca>
2020-02-27 22:04:34 -0500

    added homework

---
+++ Homework.py
@@ -0,0 +1,6 @@
+def stars(n):
+    print("*"*n)
+i=1
+while i<100:
+    stars(i)
+    i+=1
---
+++ L7E5.py
@@ -0,0 +1,7 @@
+def takeargs(n1='',n2='',n3=''):
+    alist=[n1,n2,n3]
+    print(alist)
+first=input("First thing?")
+second=input("Second thing")
+third=input("Third thing")
+takeargs(first,second,third)
